5d7afa7f2e941851820aa0f49ec29d90f4a6eb76
toctoce <toctoce@naver.com>
2026-03-20 15:53:10 +0900

    feat(OffState) : 전원 꺼져있는 상태 구현

---
+++ src/com/brunomnsilva/model/player/OffState.java
@@ -0,0 +1,37 @@
+package com.brunomnsilva.model.player;
+
+public class OffState extends MusicPlayerState{
+    public OffState(MusicPlayer player) {
+        super(player);
+    }
+
+    @Override
+    public void play() {
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    @Override
+    public void next() {
+    }
+
+    @Override
+    public void prev() {
+    }
+
+    @Override
+    public void turnOn() {
+        player.changeState(new StoppedState(player));
+    }
+
+    @Override
+    public void turnOff() {
+    }
+
+    @Override
+    public String status() {
+        return "";
+    }
+}
